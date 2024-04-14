7be2fe2e171c224f406bc3fb56dcf3588de07ff3
shalinkumar <sayhi2shalin@gmail.com>
2024-04-14 13:54:16 +0100

    Debug

--- TastyReviewsServer/Model/RestaurantImagesModelTest.cs
+++ TastyReviewsServer/Model/RestaurantImagesModelTest.cs
@@ -3,7 +3,7 @@
     public class RestaurantImagesModelTest
     {
         public Guid Guid { get; set; }
-        public bool IsInterior { get; set; }
+        //public bool IsInterior { get; set; }
 
         //public IFormFile FormImage { get; set; }
         //public Byte[] Image { get; set; }
